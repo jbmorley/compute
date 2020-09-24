13fbcfb083105d4ab7b0452ff0d5d1a829770b9e
Jason Barrie Morley <jbmorley@mac.com>
2020-09-23 17:12:04 -0700

    Move the device address method into a UIDevice extension

--- Compute/ComputeApp.swift
+++ Compute/ComputeApp.swift
@@ -25,37 +25,14 @@
 
 import SwiftUI
 
-func getIPAddress() -> String? {
-    var address: String?
-    var ifaddr: UnsafeMutablePointer<ifaddrs>? = nil
-    if getifaddrs(&ifaddr) == 0 {
-        var ptr = ifaddr
-        while ptr != nil {
-            defer { ptr = ptr?.pointee.ifa_next }
-
-            let interface = ptr?.pointee
-            let addrFamily = interface?.ifa_addr.pointee.sa_family
-            if addrFamily == UInt8(AF_INET) /* || addrFamily == UInt8(AF_INET6) */ {
-
-                if let name: String = String(cString: (interface?.ifa_name)!), name == "en0" {
-                    var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
-                    getnameinfo(interface?.ifa_addr, socklen_t((interface?.ifa_addr.pointee.sa_len)!), &hostname, socklen_t(hostname.count), nil, socklen_t(0), NI_NUMERICHOST)
-                    address = String(cString: hostname)
-                }
-            }
-        }
-        freeifaddrs(ifaddr)
-    }
-    return address
-}
-
 class Manager: ObservableObject {
+
+    var address: String?
 
     lazy var server: Server = {
         return Server(root: documentsUrl())
     }()
 
-    var address: String?
     var locations: [URL] {
         return (try? FileManager.default.contentsOfDirectory(at: documentsUrl(), includingPropertiesForKeys: nil, options: [.skipsHiddenFiles])) ?? []
     }
@@ -71,7 +48,7 @@ class Manager: ObservableObject {
 
     func start() {
         UIApplication.shared.isIdleTimerDisabled = true
-        address = getIPAddress()
+        address = UIDevice.current.address
         do {
             try server.start()
             print("Successfully started server!")
---
+++ Compute/Extensions/UIDevice+Extensions.swift
@@ -0,0 +1,56 @@
+//
+//  UIDevice+Extensions.swift
+//  Compute
+//
+//  Copyright (c) 2020 Jason Morley
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+
+import UIKit
+
+extension UIDevice {
+
+    var address: String? {
+        var address: String?
+        var ifaddr: UnsafeMutablePointer<ifaddrs>? = nil
+        if getifaddrs(&ifaddr) == 0 {
+            var ptr = ifaddr
+            while ptr != nil {
+                defer { ptr = ptr?.pointee.ifa_next }
+                let interface = ptr?.pointee
+                let addrFamily = interface?.ifa_addr.pointee.sa_family
+                if addrFamily == UInt8(AF_INET) /* || addrFamily == UInt8(AF_INET6) */ {
+                    guard let interfaceName = interface?.ifa_name else {
+                        continue
+                    }
+                    let name = String(cString: interfaceName)
+                    if name == "en0" {
+                        var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                        getnameinfo(interface?.ifa_addr, socklen_t((interface?.ifa_addr.pointee.sa_len)!), &hostname, socklen_t(hostname.count), nil, socklen_t(0), NI_NUMERICHOST)
+                        address = String(cString: hostname)
+                    }
+                }
+            }
+            freeifaddrs(ifaddr)
+        }
+        return address
+    }
+
+}
