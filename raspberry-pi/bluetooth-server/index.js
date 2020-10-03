a6a1780672bdd5892a9c834b82066413979a3caf
Jason Morley <jbmorley@mac.com>
2020-10-03 03:37:11 +0100

    Cleaning up the bluetooth server a little, and adding support for running commands.

--- raspberry-pi/bluetooth-server/index.js
+++ raspberry-pi/bluetooth-server/index.js
@@ -1,4 +1,23 @@
 const bleno = require('bleno');
+const { exec } = require('child_process')
+
+let ServiceUUID = '1803'
+let CharacteristicKeepAliveUUID = '2A06'
+let CharacteristicShutdownUUID = '2A07'
+
+function run(command) {
+    exec(command, (error, stdout, stderr) => {
+	if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+	}
+	if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+	}
+	console.log(`stdout: ${stdout}`);
+    });
+}
 
 bleno.on('stateChange', function(state) {
     console.log('on stateChange: ' + state);
@@ -16,12 +35,12 @@ bleno.on('advertisingStart', function(error) {
     bleno.setServices([
       // link loss service  
       new bleno.PrimaryService({
-        uuid: '1803', 
+        uuid: ServiceUUID, 
         characteristics: [
           // Alert Level
           new bleno.Characteristic({
             value: 0, 
-            uuid: '2A06',
+            uuid: CharacteristicKeepAliveUUID,
               properties: ['read', 'write', 'notify'],
               onReadRequest(offset, callback) {
 		  console.log("read value");
