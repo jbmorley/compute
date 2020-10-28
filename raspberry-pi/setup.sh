60b13c9924465039b1ed4a05ca235c5687d11743
Jason Morley <jbmorley@mac.com>
2020-10-28 20:52:08 +0000

    Add some more requirements.

--- raspberry-pi/setup.sh
+++ raspberry-pi/setup.sh
@@ -2,6 +2,8 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+export DEBIAN_FRONTEND=noninteractive
+
 sudo apt update
 sudo apt upgrade -y
 cat "${DIR}/requirements.txt" | xargs sudo apt-get install -y
