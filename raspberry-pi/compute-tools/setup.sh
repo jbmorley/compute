dc608b4173b0e91cc79e70fc74496fd6c16898c0
Jason Barrie Morley <jbmorley@mac.com>
2020-10-29 07:49:24 -0700

    Better support for building compue specific images.

--- raspberry-pi/compute-tools/setup.sh
+++ raspberry-pi/compute-tools/setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+set -e
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 export DEBIAN_FRONTEND=noninteractive
 
-sudo apt update
-sudo apt upgrade -y
+sudo apt-get update
+sudo apt-get upgrade -y
 cat "${DIR}/requirements.txt" | xargs sudo apt-get install -y
 
 ansible-playbook "${DIR}/setup.yaml"
